50279f4aa5612511a94a173935ce7d850a38906c
lufemas <maschietto.jr@gmail.com>
2020-06-02 12:33:06 -0400

    default cursor when stop playing

--- js/index.js
+++ js/index.js
@@ -12,11 +12,7 @@ let initDeltatime = 0
 
 window.addEventListener('mousemove', (e)=> {
     if(isRecording){
-        // console.log(e.clientX)
-        // console.log(e.clientY)
-        // console.log(e.timeStamp)
         mouseRecord.push({x: e.clientX, y: e.clientY, timeStart: e.timeStamp - initDeltatime })
-        console.log($)
     }    
 })
 
@@ -65,14 +61,13 @@ function toogleRecord(){
 
 function play(ind, deltaTime = 0){
     if(ind < mouseRecord.length){
-        console.log(`playing index: ${ind}`)
         setTimeout(()=>{
             $fakeCursor.style.transform = `translate(${mouseRecord[ind].x}px,${mouseRecord[ind].y}px)`;
             play(ind+1, mouseRecord[ind].timeStart)
         },mouseRecord[ind].timeStart - deltaTime)
     }else{
         isPlaying = false;
-        $.body.style.cursor = 'pointer'
+        $.body.style.cursor = 'default'
         $recordBtn.style.cursor = 'pointer'
 
         $playBtn.innerHTML = `PLAY`
